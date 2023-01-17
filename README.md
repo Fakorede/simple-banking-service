2e8a713a6411634db85660d3a11fe9ee3d30e322
fakorede <abiolafakorede@gmail.com>
2023-01-17 17:06:12 +0100

    Tests for accounts sql queries

--- README.md
+++ README.md
@@ -5,6 +5,16 @@
 - [Postgres Docker Image](https://hub.docker.com/_/postgres) - docker utility container for running postgresql locally
 - [Golang migrate](https://github.com/golang-migrate/migrate/tree/master/cmd/migrate) - For creating and running migrations in Golang.
 - [Sqlc](https://sqlc.dev/) - For compiling sql queries to type-safe Golang code.
+- [pq](https://pkg.go.dev/github.com/lib/pq) - Go Postgres driver for database/sql
+- [Testify](https://github.com/stretchr/testify) - A toolkit with common assertions and mocks that plays nicely with the standard library.
+
+## Run Commands
+
+A makefile has been included and contains important commands that'll come in handy ex for running migrations, compiling queries using sqlc, running tests
+
+```
+$ make test // to run tests
+```
 
 ## Built with
 
---
+++ utils/random.go
@@ -0,0 +1,53 @@
+package utils
+
+import (
+	"math/rand"
+	"strings"
+	"time"
+)
+
+const alphabets = "abcdefhijklmnopqrstuvwxyz"
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+// RandomInt generates a random integer btw min & max
+func RandomInt(min, max int64)  int64 {
+	return min + rand.Int63n(max-min+1)
+}
+
+// RandomString generates a random string
+func RandomString(n int) string {
+	var sb strings.Builder
+	k := len(alphabets)
+
+	for i := 0; i < n; i++ {
+		c := alphabets[rand.Intn(k)]
+		sb.WriteByte(c)
+	}
+
+	return sb.String()
+}
+
+// RandomOwner generates a random name
+func RandomOwner() string {
+	return RandomString(6)
+}
+
+// RandomAmount generates a random amount
+func RandomAmount() int64 {
+	return RandomInt(0, 1000)
+}
+
+// RandomCurrency generates a random currency
+func RandomCurrency() string {
+	currencies := []string{
+		"USD",
+		"GBP",
+		"EUR",
+	}
+
+	n := len(currencies)
+	return currencies[rand.Intn(n)]
+}
