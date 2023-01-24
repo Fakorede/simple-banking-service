838215c993b79f99f9cd12460a9657c1d242e978
fakorede <abiolafakorede@gmail.com>
2023-01-24 23:08:42 +0100

    Test for entry sql queries

---
+++ db/sqlc/entry_test.go
@@ -0,0 +1,69 @@
+package db
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/fakorede/simple-banking-service/utils"
+	"github.com/stretchr/testify/require"
+)
+
+func createRandomEntry(t *testing.T, account Account) Entry {
+	arg := CreateEntryParams {
+		AccountID: account.ID,
+		Amount: utils.RandomAmount(),
+	}
+
+	entry, err := testQueries.CreateEntry(context.Background(), arg)
+	require.NoError(t, err)
+	require.NotEmpty(t, entry)
+	require.Equal(t, entry.AccountID, arg.AccountID)
+	require.Equal(t, entry.Amount, arg.Amount)
+	require.NotZero(t, entry.ID)
+	require.NotZero(t, entry.CreatedAt)
+
+	return entry
+}
+
+func TestCreateEntry(t *testing.T) {
+	account := createRandomAccount(t)
+	createRandomEntry(t, account)	
+}
+
+func TestGetEntry(t *testing.T) {
+	account := createRandomAccount(t)
+	entry := createRandomEntry(t, account)
+
+	fetchedEntry, err := testQueries.GetEntry(context.Background(), entry.ID)
+	require.NoError(t, err)
+	require.NotEmpty(t, fetchedEntry)
+	require.Equal(t, fetchedEntry.ID, entry.ID)
+	require.Equal(t, fetchedEntry.Amount, entry.Amount)
+	require.Equal(t, fetchedEntry.AccountID, entry.AccountID)
+
+	require.WithinDuration(t, fetchedEntry.CreatedAt, entry.CreatedAt, time.Second)
+}
+
+func TestListEntries(t *testing.T) {
+	account := createRandomAccount(t)
+
+	for i := 0; i < 10; i++ {
+		createRandomEntry(t, account)
+	}
+
+	arg := ListEntriesParams{
+		AccountID: account.ID,
+		Limit: 5,
+		Offset: 5,
+	}
+
+	entries, err := testQueries.ListEntries(context.Background(), arg)
+	require.NoError(t, err)
+
+	require.Len(t, entries, 5)
+
+	for _, entry := range entries {
+		require.NotEmpty(t, entry)
+	}
+}
